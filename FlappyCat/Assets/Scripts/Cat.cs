f6bc470dac0027b8dc04ee60b4f30631b158c32e
lucas625 <lagc@cin.ufpe.br>
2019-09-21 23:09:27 -0300

    animation

--- FlappyCat/Assets/Scripts/Cat.cs
+++ FlappyCat/Assets/Scripts/Cat.cs
@@ -9,10 +9,13 @@ public class Cat : MonoBehaviour
     private bool isDead = false;
     private Rigidbody2D rb2d;
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class Cat : MonoBehaviour
             if(Input.GetMouseButtonDown(0)) {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
             }
         }
     }
@@ -30,6 +34,7 @@ public class Cat : MonoBehaviour
     void OnCollisionEnter2D()
     {
         isDead = true;
+        anim.SetTrigger("Die");
     }
 
 }
