8f892c85a6b29c59fd702702c939666390dff9b7
lucas625 <lagc@cin.ufpe.br>
2019-09-22 22:27:56 -0300

    game controller

--- FlappyCat/Assets/Scripts/Cat.cs
+++ FlappyCat/Assets/Scripts/Cat.cs
@@ -8,7 +8,6 @@ public class Cat : MonoBehaviour
     public float upForce = 200;
     private bool isDead = false;
     private Rigidbody2D rb2d;
-
     private Animator anim;
 
     // Start is called before the first frame update
@@ -21,8 +20,10 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isDead) {
-            if(Input.GetMouseButtonDown(0)) {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
                 anim.SetTrigger("Flap");
@@ -35,6 +36,7 @@ public class Cat : MonoBehaviour
     {
         isDead = true;
         anim.SetTrigger("Die");
+        GameController.instance.CatDied();
     }
 
 }
---
+++ FlappyCat/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public GameObject GameOverText;
+    public bool GameOver = false;
+
+    // Awake is called before any Start
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameOver && Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void CatDied()
+    {
+        GameOverText.SetActive(true);
+        GameOver = true;
+    }
+
+
+}
